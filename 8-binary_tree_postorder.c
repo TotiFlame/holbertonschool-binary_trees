b16ccd948d5edea79d395f970fa6b8098f626925
TotiFlame <santifer4321@gmail.com>
2022-09-02 08:05:06 -0700

    Exercise 8 second attempt

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -6,7 +6,7 @@
  * @func: Function to execute with each node.
  */
 
-void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int));
 {
 	if (tree && func)
 	{
