19e4c069147644e8275946921c9e16cd6858c034
TotiFlame <santifer4321@gmail.com>
2022-09-02 08:11:15 -0700

    Exercise 8 betty style

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,7 +1,8 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_inorder - Goes through a binary tree using post-order traversal.
+ * binary_tree_postorder - Goes through a binary tree using
+ * post-order traversal.
  * @tree: Tree input.
  * @func: Function to execute with each node.
  */
@@ -11,7 +12,7 @@ void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
 	if (tree && func)
 	{
 		binary_tree_postorder(tree->left, func);
-		binary_tree_postorder(tree->right, func);	
+		binary_tree_postorder(tree->right, func);
 		(*func)(tree->n);
 	}
 }
