62bcf60835d86b7a6248e438b8c64c596d1dc2f7
TotiFlame <santifer4321@gmail.com>
2022-09-02 15:29:54 -0700

    Exercise 17 first attempt

---
+++ 17-binary_tree_sibling.c
@@ -0,0 +1,25 @@
+#include "binary_trees.h"
+/**
+* binary_tree_sibling - finds the sibling of a node
+*
+* @node: pointer to the node to find the sibling
+*
+* Return: pointer to the sibling node or NULL
+*/
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent = NULL;
+
+	if (!node || !node->parent)
+		return (NULL);
+	parent = node->parent;
+	if (parent->left || parent->left == node)
+	{
+		if (parent->right)
+			return (parent->right);
+		else
+			return (NULL);
+	}
+	else
+		return (parent->left);
+}
