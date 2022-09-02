3a487059bc63ea0d52efebfd4f0f3be0f8a74772
Blas <blashernandez98@gmail.com>
2022-09-02 10:56:36 -0700

    Task14

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_balance - Function that calculates balance factor of a tree.
+ * @tree: Tree input.
+ *
+ * Return: Balance factor of tree.
+ */
+
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	size_t left = 0, right = 0;
+
+	if (tree)
+	{
+		if (tree->left)
+			left = 1 + binary_tree_height(tree->left);
+		if (tree->right)
+			right = 1 + binary_tree_height(tree->right);
+		return (left - right);
+	}
+	return (0);
+}
