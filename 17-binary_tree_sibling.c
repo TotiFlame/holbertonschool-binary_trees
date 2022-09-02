2074604ca3076108594b36e3986f53751f09b529
TotiFlame <santifer4321@gmail.com>
2022-09-02 15:41:46 -0700

    Exercise 17 second attempt

--- 17-binary_tree_sibling.c
+++ 17-binary_tree_sibling.c
@@ -13,13 +13,17 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node)
 	if (!node || !node->parent)
 		return (NULL);
 	parent = node->parent;
-	if (parent->left || parent->left == node)
+	if (parent->left)
 	{
-		if (parent->right)
-			return (parent->right);
+		if (parent->left == node)
+		{
+			if (parent->right)
+				return (parent->right);
+			else
+				return (NULL);
+		}
 		else
-			return (NULL);
+			return (parent->left);
 	}
-	else
-		return (parent->left);
+	return (NULL);
 }
