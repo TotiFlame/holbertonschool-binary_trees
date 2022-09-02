088149a2826ccb8dc48d21f4e1f7a5a50b517c0f
TotiFlame <santifer4321@gmail.com>
2022-09-02 08:00:58 -0700

    Exercise 8 first attempt

---
+++ 8-binary_tree_postorder.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Goes through a binary tree using post-order traversal.
+ * @tree: Tree input.
+ * @func: Function to execute with each node.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		binary_tree_inorder(tree->left, func);
+		binary_tree_inorder(tree->right, func);	
+		(*func)(tree->n);
+	}
+}
