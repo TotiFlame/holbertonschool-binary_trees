c9ed15228b5869d559a3b5d41292b5ea87d66f88
TotiFlame <santifer4321@gmail.com>
2022-09-02 09:30:50 -0700

    Exercise 9 second attempt

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -13,14 +13,8 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (!tree)
 		return (0);
 	if (tree->left)
-	{
-		l++;
-		binary_tree_height(tree->left);
-	}
+		l = 1 + binary_tree_height(tree->left);
 	if (tree->right)
-	{
-		r++;
-		binary_tree_height(tree->right);
-	}
-	return (l + r);
+		r = 1 + binary_tree_height(tree->right);
+	return (l > r ? l : r);
 }
